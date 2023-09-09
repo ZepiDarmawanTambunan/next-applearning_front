c824606b043d3c6cde62a6e11d1e794507ccbf33
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-09-09 21:12:35 +0700

    config commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withImages = require('next-images');
+const withReactSvg = require('next-react-svg');
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = withImages(
+  withReactSvg({
+    webpack(config, options) {
+      return config;
+    },
+    reactSvg: {
+      include: path.resolve(__dirname, './public/images'), // Sesuaikan dengan direktori SVG Anda
+    },
+    images: {
+      domains: ['example.com'], // Ganti dengan domain yang sesuai
+      disableStaticImages: false,
+      formats: ['image/jpeg', 'image/png', 'image/webp'],
+      deviceSizes: [320, 420, 768, 1024, 1200],
+      imageSizes: [16, 32, 48, 64, 96],
+    },
+  })
+);
+
+// Konfigurasi Next.js lainnya
+module.exports.reactStrictMode = true;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Head from 'next/head'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
